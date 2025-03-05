4c2a2997645a48dc8865b960840cec83428789c5
Sohan1928 <sohanurrahman1928@gmail.com>
2025-03-05 18:02:50 +0600

    footer section updated

--- script.js
+++ script.js
@@ -101,29 +101,32 @@ sliderAnimation();
 
 var tl = gsap.timeline();
 
-tl.from("#loader h3", {
-  opacity: 0,
-  x: 40,
-  duration: 1,
-  stagger: 0.1,
-});
-tl.to("#loader h3", {
-  opacity: 0,
-  x: -10,
-  duration: 1,
-  stagger: 0.1,
-});
+function loaderAnimation() {
+  tl.from("#loader h3", {
+    opacity: 0,
+    x: 40,
+    duration: 1,
+    stagger: 0.1,
+  });
+  tl.to("#loader h3", {
+    opacity: 0,
+    x: -10,
+    duration: 1,
+    stagger: 0.1,
+  });
 
-tl.to("#loader", {
-  opacity: 0,
-});
-tl.from("#page1-content h1 span", {
-  y: 100,
-  opacity: 0,
-  stagger: 0.1,
-  duration: 0.5,
-  delay: -0.5,
-});
-tl.to("#loader", {
-  display: "none",
-});
+  tl.to("#loader", {
+    opacity: 0,
+  });
+  tl.from("#page1-content h1 span", {
+    y: 100,
+    opacity: 0,
+    stagger: 0.1,
+    duration: 0.5,
+    delay: -0.5,
+  });
+  tl.to("#loader", {
+    display: "none",
+  });
+}
+loaderAnimation();
